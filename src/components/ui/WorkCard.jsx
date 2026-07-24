563f87275f96429ea49f2bc8d7891af7c350a928
Sonu <sonurao.skr@gmail.com>
2026-07-24 10:21:21 +0530

    fixing

--- src/components/ui/WorkCard.jsx
+++ src/components/ui/WorkCard.jsx
@@ -48,7 +48,7 @@ export default function WorkCard({
         {/* Video */}
         <video
           ref={videoRef}
-          src={videoLink || "/videos/corporateVideo.mp4"}
+          src={videoLink}
           muted
           loop
           playsInline
